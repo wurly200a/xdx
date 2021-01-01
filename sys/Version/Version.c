82a34e6eb4ae6d0048acece1b00f724ac8bc2195
Yushi Oka <indigo@sub.nifty.jp>
2021-01-01 16:50:16 +0900

    version 4.00

--- sys/Version/Version.c
+++ sys/Version/Version.c
@@ -9,7 +9,7 @@
 /* “à•”ŠÖ”’è‹` */
 #include "Version.h"
 /* “à•”•Ï”’è‹` */
-static WORD versionData = 0x0310;
+static WORD versionData = 0x0400;
 static TCHAR szVersion[64];
 
 /********************************************************************************
